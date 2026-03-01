cf84b0ca1ee1aadc30c49be2d6174df8f79eac13
Jason Sosa <jesonsosa04.js@gmail.com>
2026-02-28 22:59:00 -0800

    I moved the location of Functions.java

--- src/main/java/edu/elac/cs131/group5/Functions.java
+++ src/main/java/edu/elac/cs131/group5/Functions.java
@@ -1,21 +1,21 @@
 public class Functions {
     public static double linear( double x){
-        /*f(x)= 2x */
+        // f(x)= 2x 
         return 2 * x; 
     }
 
     public static double quadratic( double x){
-       /* f(x)= x^2 */
+       // f(x)= x^2 
         return x * x; 
     }
 
     public static double exponential( double x){
-        /* f(x)= 2^x */
+        // f(x)= 2^x 
         return Math.pow(2,x); 
     }
     
     public static double logarithmic( double x){
-        /* f(x)= log2(x) */
+        // f(x)= log2(x) 
         if (x <= 0) {
             return Double.NaN;
         }
@@ -25,7 +25,7 @@ public class Functions {
     /* functionName = "linear", "quadratic", "exponential", "logarithmic"
      * minX = minimum x value start of the graph
      * maxX = maximum x value end of the graph
-     * numPoints = number of points to generate       
+     numPoints = number of points to generate       
     */
     public static double[][] GraphPoints(String functionName, double minX, double maxX, int numPoints) {
         double[][] points = new double[numPoints][2];
@@ -56,4 +56,14 @@ public class Functions {
         }
         return points;
     }
+    /*  Test individual functions
+        public static void main(String[] args) {
+
+        System.out.println("f(x) = 2x: f(2) = " + linear(2));
+        System.out.println("f(x) = x²: f(3) = " + quadratic(3));
+        System.out.println("f(x) = 2^x: f(5) = " + exponential(5));
+        System.out.println("f(x) = log₂(x): f(8) = " + logarithmic(8));
+        System.out.println();
+        }
+    */    
 }
